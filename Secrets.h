596c6f8a47cb61f29e5ce6922fc04104702c849b
Tom Rolander <rolander@stanford.edu>
2021-08-05 16:44:43 -0700

    added GoPro 9 values

--- Secrets.h
+++ Secrets.h
@@ -2,8 +2,16 @@
 #define SECRETS_H
 
 // Replace the following:
+#if 1
 #define GOPRO_SSID "GP28073633"
 #define GOPRO_PASS "B3W-dGc-5N7"
 #define CAMERA 7
+#endif
+
+#if 0
+#define GOPRO_SSID "GP25909736"
+#define GOPRO_PASS "sky-MD5-jRB"
+#define CAMERA 9
+#endif
 
 #endif
